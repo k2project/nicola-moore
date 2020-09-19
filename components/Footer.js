26db0bbb4e8606171a1374b91a06818e4fcb3ffb
k2projct <kopczkrzy@gmail.com>
2020-09-19 13:15:52 +0100

    message component styling

--- components/Footer.js
+++ components/Footer.js
@@ -7,7 +7,11 @@ export default function Footer({ contact }) {
     const [details, setDetails] = useState('');
 
     const [error, setError] = useState(null);
-    const [message, setMessage] = useState(null);
+    const [message, setMessage] = useState({
+        text:
+            'Message sent successfully.Please provide either a valid email address or a mobile in digital format with optional country code eg. +44 ',
+        status: 'success',
+    });
 
     const validateEmail = (str) => details.includes('@');
 
