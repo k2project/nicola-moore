2e67ccef636ea6ee2520e3e230ee183b980c9c5c
k2projct <kopczkrzy@gmail.com>
2020-11-23 21:39:58 +0000

    new Top design

--- components/Top.js
+++ components/Top.js
@@ -3,35 +3,22 @@ import React from 'react';
 export default function Top({ children }) {
     return (
         <section className='top' id='top'>
-            <img
-                src='/imgs/bg_home_2.2.png'
-                alt=''
-                className='top-rel desktop'
-                rel="preload"
-            />
-            <div>
-                <img
-                    src='/imgs/bg_home_3.2_right.png'
-                    alt=''
-                    className='top-rel desktop side'
-                    rel="preload"
-                />
-                <img
-                    src='/imgs/bg_home_3.2_left.png'
-                    alt=''
-                    className='top-abs desktop side'
-                    rel="preload"
-                />
-                <img src='/imgs/tablet_top.png' alt='' className='tablet' rel="preload"/>
-                <div className='top-abs top-strapline desktop'>{children}</div>
-                <img src='/imgs/tablet_btm.png' alt='' className='tablet'rel="preload" />
-            </div>
+             <div className='top-strapline'>{children}</div>
+            
             <img
                 src='/imgs/bg_home_4.2.png'
                 alt=''
-                className='top-rel desktop'
-                rel="preload"
+                className='desktop'
+            /> 
+            <img
+                src='/imgs/bg_home_2.2.png'
+                alt=''
+                className='desktop'
             />
+            <img src='/imgs/tablet_top.png' alt='' className='mobile' />
+
+            <img src='/imgs/tablet_btm.png' alt='' className='mobile'/>
+           
         </section>
     );
 }
