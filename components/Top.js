3d0ea725dca0de9137303595bac36d297c298f0d
k2projct <kopczkrzy@gmail.com>
2020-11-26 21:33:21 +0000

    img centered

--- components/Top.js
+++ components/Top.js
@@ -3,22 +3,23 @@ import React from 'react';
 export default function Top({ children }) {
     return (
         <section className='top' id='top'>
+
+           
              <div className='top-strapline'>{children}</div>
             
             <img
-                src='/imgs/bg_home_4.2.png'
+                src='/imgs/bg_home_4.2.2.png'
                 alt=''
                 className='desktop'
             /> 
             <img
-                src='/imgs/bg_home_2.2.png'
+                src='/imgs/bg_home_2.2.2.png'
                 alt=''
                 className='desktop'
             />
             <img src='/imgs/tablet_top.png' alt='' className='mobile' />
 
             <img src='/imgs/tablet_btm.png' alt='' className='mobile'/>
-           
         </section>
     );
 }
